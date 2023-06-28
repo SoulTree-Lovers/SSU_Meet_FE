9c013010ee1ff83241da9c8e1edc852188a1537e
강승민 <rkdtmdals999@naver.com>
2023-06-28 23:05:43 +0900

    Create GridView PostIt

--- ssu_meet/lib/widgets/mobile_layout.dart
+++ ssu_meet/lib/widgets/mobile_layout.dart
@@ -29,6 +29,30 @@ class _MobileLayoutState extends State<MobileLayout> {
           backgroundColor: Colors.blue[100],
           foregroundColor: Colors.white,
         ),
+        body: GridView.builder(
+          shrinkWrap: true,
+          itemCount: 100,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
+            childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
+            mainAxisSpacing: 10, //수평 Padding
+            crossAxisSpacing: 10, //수직 Padding
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            // return Text(index.toString());
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                width: 150,
+                height: 150,
+                decoration: const BoxDecoration(
+                  color: Colors.amber,
+                ),
+                child: Text("$index"),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
