aa7484dcebbf250f8021cf3087d2994cf0bbf34f
강승민 <rkdtmdals999@naver.com>
2023-06-29 23:07:28 +0900

    모바일 버전 화면 UI 구현
    1. 보유 코인 개수
    2. 포스트잇 제목과 등록 개수
    3. 등록 버튼 (GestureDetector를 통해 동작)
    4. 포스트잇 텍스트와 포스트잇 목록을 선으로 구분
    5. main에서 initial_page가 아닌 responsive_page로 바로 이동하게 변경

--- ssu_meet/lib/widgets/mobile_layout.dart
+++ ssu_meet/lib/widgets/mobile_layout.dart
@@ -12,6 +12,11 @@ class MobileLayout extends StatefulWidget {
 class _MobileLayoutState extends State<MobileLayout> {
   @override
   Widget build(BuildContext context) {
+    int coinNumber = 3; // 테스트용
+    int postitNumber = 15; // 테스트용
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return WillPopScope(
       onWillPop: () async {
         return false;
@@ -29,29 +34,111 @@ class _MobileLayoutState extends State<MobileLayout> {
           backgroundColor: Colors.blue[100],
           foregroundColor: Colors.white,
         ),
-        body: GridView.builder(
-          shrinkWrap: true,
-          itemCount: 100,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-            childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
-            mainAxisSpacing: 10, //수평 Padding
-            crossAxisSpacing: 10, //수직 Padding
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            // return Text(index.toString());
-            return Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                width: 150,
-                height: 150,
-                decoration: const BoxDecoration(
-                  color: Colors.amber,
-                ),
-                child: Text("$index"),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      "보유 코인: $coinNumber",
+                      style: const TextStyle(
+                        fontFamily: "Nanum_1",
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            );
-          },
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        const Text(
+                          "포스트잇",
+                          style: TextStyle(
+                            fontFamily: "Ongeul",
+                            fontSize: 50,
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "등록 개수: $postitNumber",
+                          style: const TextStyle(
+                            fontFamily: "Nanum_1",
+                            fontSize: 20,
+                          ),
+                        ),
+                      ],
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        print("등록버튼터치");
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.all(10),
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.blue,
+                          ),
+                          borderRadius: BorderRadius.circular(45),
+                        ),
+                        child: const Text(
+                          "+ 등록",
+                          style: TextStyle(
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Container(
+                  width: screenWidth * 0.95,
+                  height: 1,
+                  color: Colors.black,
+                ),
+              ),
+              SizedBox(
+                height: screenHeight * 0.7,
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  itemCount: 100,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
+                    childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
+                    mainAxisSpacing: 10, //수평 Padding
+                    crossAxisSpacing: 10, //수직 Padding
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    // return Text(index.toString());
+                    return Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        width: 150,
+                        height: 150,
+                        decoration: const BoxDecoration(
+                          color: Colors.amber,
+                        ),
+                        child: Text("$index"),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
