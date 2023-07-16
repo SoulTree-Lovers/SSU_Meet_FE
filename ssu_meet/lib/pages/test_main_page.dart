0191c450c3b43f5d5b75fdfdc690c1960bcab9c7
강승민 <rkdtmdals999@naver.com>
2023-07-16 14:29:15 +0900

    Json Data 임시로 저장한 후 포스트잇에 내용 출력

--- ssu_meet/lib/pages/test_main_page.dart
+++ ssu_meet/lib/pages/test_main_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class TestMainPage extends StatefulWidget {
   const TestMainPage({super.key});
@@ -31,23 +34,28 @@ class _TestMainPageState extends State<TestMainPage> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Container(
-                    width: 90,
-                    height: 40,
-                    decoration: ShapeDecoration(
-                      color: Colors.white.withOpacity(0.5),
-                      shape: RoundedRectangleBorder(
-                        side: const BorderSide(
-                            width: 1, color: Color(0xFF010101)),
-                        borderRadius: BorderRadius.circular(20),
+                  GestureDetector(
+                    onTap: () {
+                      print("등록하기 버튼 클릭 !");
+                    },
+                    child: Container(
+                      width: 90,
+                      height: 40,
+                      decoration: ShapeDecoration(
+                        color: Colors.white.withOpacity(0.5),
+                        shape: RoundedRectangleBorder(
+                          side: const BorderSide(
+                              width: 1, color: Color(0xFF010101)),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
                       ),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        "+ 등록하기",
-                        style: TextStyle(
-                          fontFamily: "Ongeul",
-                          fontSize: 20,
+                      child: const Center(
+                        child: Text(
+                          "+ 등록하기",
+                          style: TextStyle(
+                            fontFamily: "Ongeul",
+                            fontSize: 20,
+                          ),
                         ),
                       ),
                     ),
@@ -55,53 +63,131 @@ class _TestMainPageState extends State<TestMainPage> {
                 ],
               ),
             ),
-            SizedBox(
-              height: screenHeight * 0.7,
-              child: GridView.builder(
-                shrinkWrap: true,
-                itemCount: 100,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-                  childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
-                  mainAxisSpacing: 10, //수평 Padding
-                  crossAxisSpacing: 10, //수직 Padding
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  // return Text(index.toString());
-                  return Center(
-                    child: SizedBox(
-                      height: screenWidth * 0.4,
-                      width: screenWidth * 0.4,
-                      child: const Stack(
-                        alignment: Alignment.center,
-                        children: <Widget>[
-                          // First image
+            FutureBuilder(
+              future: getPostItFrontData(),
+              builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  // While the future is not yet completed
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  // If an error occurred
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  // If the future completed successfully
+                  return SizedBox(
+                    height: screenHeight * 0.7,
+                    child: GridView.builder(
+                      shrinkWrap: true,
+                      itemCount: snapshot.data!.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
+                        childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
+                        mainAxisSpacing: 10, //수평 Padding
+                        crossAxisSpacing: 10, //수직 Padding
+                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        var postIt = snapshot.data![index];
+                        var nickname = postIt["nickname"];
+                        var age = postIt["age"];
+                        var mbti = postIt["mbti"];
+                        var hobby = postIt["hobby"];
+                        // return Text(index.toString());
+                        // Future<String> nickname = getPostItFrontNickname(index);
+                        // var age = getPostItFrontData(index)["age"];
+                        // var mbti = getPostItFrontData(index)["mbti"];
+                        // var hobby = getPostItFrontData(index)["hobby"];
+                        return GestureDetector(
+                          onTap: () {
+                            print("Tapped Post It $index");
+                          },
+                          child: Center(
+                            child: Container(
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: <Widget>[
+                                  // First image
 
-                          Image(
-                              image: AssetImage(
-                                  "assets/images/images2/yellowpostit.png"),
-                              width: 250,
-                              height: 250),
+                                  const Image(
+                                      image: AssetImage(
+                                          "assets/images/images2/yellowpostit.png"),
+                                      width: 250,
+                                      height: 250),
 
-                          // Second image overlapped on top of the first image
-                          Positioned(
-                            left: 60,
-                            top: 0,
-                            child: Image(
-                              image: AssetImage(
-                                  "assets/images/images2/tape10.png"),
+                                  // Second image overlapped on top of the first image
+                                  const Positioned(
+                                    left: 80,
+                                    top: 0,
+                                    child: Image(
+                                      image: AssetImage(
+                                          "assets/images/images2/tape10.png"),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 70,
+                                    child: SizedBox(
+                                      height: 100,
+                                      width: 100,
+                                      child: SingleChildScrollView(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              "닉네임: $nickname",
+                                              style: const TextStyle(
+                                                fontFamily: "Nanum_Ogbice",
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                            Text(
+                                              "나이: $age",
+                                              style: const TextStyle(
+                                                fontFamily: "Nanum_Ogbice",
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                            Text(
+                                              "MBTI: $mbti",
+                                              style: const TextStyle(
+                                                fontFamily: "Nanum_Ogbice",
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                            Text(
+                                              "취미: $hobby",
+                                              style: const TextStyle(
+                                                fontFamily: "Nanum_Ogbice",
+                                                fontSize: 20,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
                   );
-                },
-              ),
+                }
+              },
             ),
           ],
         ),
       ),
     );
   }
+}
+
+Future<List> getPostItFrontData() async {
+  // 서버에서 포스트잇 앞면 데이터 가져오기
+  String jsonString = await rootBundle.loadString('json/post_it_json.json');
+  final jsonResponse = json.decode(jsonString);
+
+  return jsonResponse;
 }
