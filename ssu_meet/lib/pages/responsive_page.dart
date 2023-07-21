19f9fd707b5898e9fa40ad75692d35ac24cc4e63
강승민 <rkdtmdals999@naver.com>
2023-07-22 00:11:01 +0900

    AppBar의 보유코인에 이미지 추가

--- ssu_meet/lib/pages/responsive_page.dart
+++ ssu_meet/lib/pages/responsive_page.dart
@@ -35,7 +35,7 @@ class _ResponsiveWebLayoutState extends State<ResponsiveWebLayout> {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Container(
-                width: 100,
+                width: 110,
                 height: 30,
                 decoration: BoxDecoration(
                   color: const Color(0xFFD7D7D7),
@@ -49,14 +49,28 @@ class _ResponsiveWebLayoutState extends State<ResponsiveWebLayout> {
                     )
                   ],
                 ),
-                child: Text(
-                  "보유 코인: $coins",
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    color: Color(0xFF1A1A1A),
-                    fontFamily: "Nanum_Ogbice",
-                    fontSize: 20,
-                  ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 25,
+                      height: 25,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage("images/currency_dollar.png"),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "보유 코인: $coins",
+                      style: const TextStyle(
+                        color: Color(0xFF1A1A1A),
+                        fontFamily: "Nanum_Ogbice",
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
