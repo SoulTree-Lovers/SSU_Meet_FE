6e221121b45cebe336393e2c07a98840c4e9da50
강승민 <rkdtmdals999@naver.com>
2023-07-28 22:31:32 +0900

    Main Page 배경 이미지 잘림 현상 해결

--- ssu_meet/lib/pages/main_page.dart
+++ ssu_meet/lib/pages/main_page.dart
@@ -26,106 +26,104 @@ class _MainPageState extends State<MainPage> {
       body: Stack(
         alignment: Alignment.center,
         children: [
-          SingleChildScrollView(
-            child: Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("images/paper_background.png"),
-                  fit: BoxFit.cover,
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/paper_background.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-              child: Column(
-                children: [
-                  const SizedBox(
-                    height: 20,
+                // Padding(
+                //   padding: const EdgeInsets.all(20.0),
+                //   child: Row(
+                //     mainAxisAlignment: MainAxisAlignment.end,
+                //     children: [
+                //       GestureDetector(
+                //         onTap: () {
+                //           print("등록하기 버튼 클릭 !");
+                //         },
+                //         child: Container(
+                //           width: 90,
+                //           height: 40,
+                //           decoration: ShapeDecoration(
+                //             color: Colors.white.withOpacity(0.5),
+                //             shape: RoundedRectangleBorder(
+                //               side: const BorderSide(
+                //                   width: 1, color: Color(0xFF010101)),
+                //               borderRadius: BorderRadius.circular(20),
+                //             ),
+                //           ),
+                //           child: const Center(
+                //             child: Text(
+                //               "+ 등록하기",
+                //               style: TextStyle(
+                //                 fontFamily: "Ongeul",
+                //                 fontSize: 20,
+                //               ),
+                //             ),
+                //           ),
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                // ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      DocumentDuplicateImage(screenWidth),
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        "나의 포스트잇: $myPostIt 개",
+                        style: TextStyle(
+                          fontFamily: "NanumSquareRoundR",
+                          fontSize: screenWidth * 0.03,
+                        ),
+                      ),
+                      VerticalLineImage(),
+                      DocumentDuplicateImage(screenWidth),
+                      const SizedBox(
+                        width: 3,
+                      ),
+                      Text(
+                        "현재 등록된 포스트잇: $totalPostIt 장",
+                        style: TextStyle(
+                          fontFamily: "NanumSquareRoundR",
+                          fontSize: screenWidth * 0.03,
+                        ),
+                      ),
+                    ],
                   ),
-                  // Padding(
-                  //   padding: const EdgeInsets.all(20.0),
-                  //   child: Row(
-                  //     mainAxisAlignment: MainAxisAlignment.end,
-                  //     children: [
-                  //       GestureDetector(
-                  //         onTap: () {
-                  //           print("등록하기 버튼 클릭 !");
-                  //         },
-                  //         child: Container(
-                  //           width: 90,
-                  //           height: 40,
-                  //           decoration: ShapeDecoration(
-                  //             color: Colors.white.withOpacity(0.5),
-                  //             shape: RoundedRectangleBorder(
-                  //               side: const BorderSide(
-                  //                   width: 1, color: Color(0xFF010101)),
-                  //               borderRadius: BorderRadius.circular(20),
-                  //             ),
-                  //           ),
-                  //           child: const Center(
-                  //             child: Text(
-                  //               "+ 등록하기",
-                  //               style: TextStyle(
-                  //                 fontFamily: "Ongeul",
-                  //                 fontSize: 20,
-                  //               ),
-                  //             ),
-                  //           ),
-                  //         ),
-                  //       ),
-                  //     ],
-                  //   ),
-                  // ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        DocumentDuplicateImage(screenWidth),
-                        const SizedBox(
-                          width: 3,
-                        ),
-                        Text(
-                          "나의 포스트잇: $myPostIt 개",
-                          style: TextStyle(
-                            fontFamily: "NanumSquareRoundR",
-                            fontSize: screenWidth * 0.03,
-                          ),
-                        ),
-                        VerticalLineImage(),
-                        DocumentDuplicateImage(screenWidth),
-                        const SizedBox(
-                          width: 3,
-                        ),
-                        Text(
-                          "현재 등록된 포스트잇: $totalPostIt 장",
-                          style: TextStyle(
-                            fontFamily: "NanumSquareRoundR",
-                            fontSize: screenWidth * 0.03,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  FutureBuilder(
-                    future: getPostItFrontData(),
-                    builder:
-                        (BuildContext context, AsyncSnapshot<List> snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        // While the future is not yet completed
-                        return const CircularProgressIndicator();
-                      } else if (snapshot.hasError) {
-                        // If an error occurred
-                        return Text('Error: ${snapshot.error}');
-                      } else {
-                        // If the future completed successfully
-                        return MainPagePostIt(
-                          screenHeight,
-                          snapshot,
-                          screenWidth,
-                        );
-                      }
-                    },
-                  ),
-                ],
-              ),
+                ),
+                FutureBuilder(
+                  future: getPostItFrontData(),
+                  builder:
+                      (BuildContext context, AsyncSnapshot<List> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      // While the future is not yet completed
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.hasError) {
+                      // If an error occurred
+                      return Text('Error: ${snapshot.error}');
+                    } else {
+                      // If the future completed successfully
+                      return MainPagePostIt(
+                        screenHeight,
+                        snapshot,
+                        screenWidth,
+                      );
+                    }
+                  },
+                ),
+              ],
             ),
           ),
           // RegistrationButtonWhite(screenHeight, screenWidth),
