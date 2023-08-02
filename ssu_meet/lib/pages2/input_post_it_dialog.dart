2c9663cc84870245d597d14ffafb485c205e6e43
sunjae0902 <sunjaegg0902@naver.com>
2023-08-02 17:55:50 +0900

    글자 수 제한 기능을 포함한 textformfield 클래스 리팩토링

--- ssu_meet/lib/pages2/input_post_it_dialog.dart
+++ ssu_meet/lib/pages2/input_post_it_dialog.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ssu_meet/pages2/select_idealtype_modal.dart';
+import 'package:ssu_meet/widgets/custom_textformfield_with_limit_letters.dart';
 
 class InputPostIt extends StatefulWidget {
   const InputPostIt({Key? key}) : super(key: key);
@@ -39,7 +40,7 @@ class _InputPostIt extends State<InputPostIt> {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
-    double screenHeight = MediaQuery.of(context).size.height;
+   // double screenHeight = MediaQuery.of(context).size.height;
     return AlertDialog(
       titlePadding: EdgeInsets.zero,
       contentPadding: EdgeInsets.zero,
@@ -370,97 +371,3 @@ class _InputPostIt extends State<InputPostIt> {
   }
 }
 
-class MyFormField extends StatefulWidget {
-  final String hintText;
-  final double screenWidth;
-  final FormFieldValidator? validator;
-  final FormFieldSetter? onSaved;
-  final int maxLength;
-  final int? maxLine;
-
-  const MyFormField({
-    Key? key,
-    required this.hintText,
-    required this.screenWidth,
-    required this.maxLength,
-    required this.maxLine,
-    this.validator,
-    this.onSaved,
-  }) : super(key: key);
-
-  @override
-  State<MyFormField> createState() => _MyFormFieldState();
-}
-
-class _MyFormFieldState extends State<MyFormField> {
-  final TextEditingController _textController = TextEditingController();
-
-  @override
-  void dispose() {
-    _textController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      keyboardType: TextInputType.text,
-      maxLines: widget.maxLine,
-      maxLength: widget.maxLength,
-      cursorWidth: 1.5,
-      cursorHeight: widget.screenWidth * 0.03,
-      cursorColor: Colors.transparent,
-      style: TextStyle(
-        fontSize: widget.screenWidth * 0.04,
-        fontFamily: "Nanum_Ogbice",
-        color: Colors.black,
-      ),
-      decoration: InputDecoration(
-        errorStyle: TextStyle(fontSize: widget.screenWidth * 0.02),
-        hintText: widget.hintText,
-        hintStyle: TextStyle(
-          fontSize: widget.screenWidth * 0.04,
-        ),
-        focusedErrorBorder: UnderlineInputBorder(
-          borderSide: BorderSide(
-              color: Colors.black, width: widget.screenWidth * 0.0015),
-        ),
-        errorBorder: UnderlineInputBorder(
-          borderSide: BorderSide(
-              color: Colors.black, width: widget.screenWidth * 0.0015),
-        ),
-        disabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.transparent,
-            width: widget.screenWidth * 0.0015,
-          ),
-        ),
-        enabledBorder: UnderlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.black,
-            width: widget.screenWidth * 0.0015,
-          ),
-        ),
-        focusedBorder: UnderlineInputBorder(
-          borderSide: BorderSide(
-            color: Colors.black,
-            width: widget.screenWidth * 0.0015,
-          ),
-        ),
-      ),
-      validator: widget.validator,
-      onSaved: widget.onSaved,
-      controller: _textController,
-      onChanged: (text) {
-        if (text.length > widget.maxLength) {
-          // 최대 글자 수를 넘으면 입력을 막습니다.
-          _textController.text = text.substring(0, widget.maxLength);
-          _textController.selection = TextSelection.fromPosition(
-            TextPosition(offset: _textController.text.length),
-          );
-        }
-      },
-      autovalidateMode: AutovalidateMode.onUserInteraction,
-    );
-  }
-}
---
+++ ssu_meet/lib/widgets/custom_textformfield_with_limit_letters.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+class MyFormField extends StatefulWidget {
+  final String hintText;
+  final double screenWidth;
+  final FormFieldValidator? validator;
+  final FormFieldSetter? onSaved;
+  final int maxLength;
+  final int? maxLine;
+
+  const MyFormField({
+    Key? key,
+    required this.hintText,
+    required this.screenWidth,
+    required this.maxLength,
+    required this.maxLine,
+    this.validator,
+    this.onSaved,
+  }) : super(key: key);
+
+  @override
+  State<MyFormField> createState() => _MyFormFieldState();
+}
+
+class _MyFormFieldState extends State<MyFormField> {
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      keyboardType: TextInputType.text,
+      maxLines: widget.maxLine,
+      maxLength: widget.maxLength,
+      cursorWidth: 1.5,
+      cursorHeight: widget.screenWidth * 0.03,
+      cursorColor: Colors.transparent,
+      style: TextStyle(
+        fontSize: widget.screenWidth * 0.04,
+        fontFamily: "Nanum_Ogbice",
+        color: Colors.black,
+      ),
+      decoration: InputDecoration(
+        errorStyle: TextStyle(fontSize: widget.screenWidth * 0.02),
+        hintText: widget.hintText,
+        hintStyle: TextStyle(
+          fontSize: widget.screenWidth * 0.04,
+        ),
+        focusedErrorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+              color: Colors.black, width: widget.screenWidth * 0.0015),
+        ),
+        errorBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+              color: Colors.black, width: widget.screenWidth * 0.0015),
+        ),
+        disabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.transparent,
+            width: widget.screenWidth * 0.0015,
+          ),
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
+            width: widget.screenWidth * 0.0015,
+          ),
+        ),
+        focusedBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black,
+            width: widget.screenWidth * 0.0015,
+          ),
+        ),
+      ),
+      validator: widget.validator,
+      onSaved: widget.onSaved,
+      controller: _textController,
+      onChanged: (text) {
+        if (text.length > widget.maxLength) {
+          // 최대 글자 수를 넘으면 입력을 막습니다.
+          _textController.text = text.substring(0, widget.maxLength);
+          _textController.selection = TextSelection.fromPosition(
+            TextPosition(offset: _textController.text.length),
+          );
+        }
+      },
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+    );
+  }
+}
