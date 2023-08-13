d20bddb450cfe165e3ec321d9a947debfb77e27d
강승민 <rkdtmdals999@naver.com>
2023-08-13 22:50:28 +0900

    Main Page GridView 높이 변경

--- ssu_meet/lib/pages/main_page.dart
+++ ssu_meet/lib/pages/main_page.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:ssu_meet/dialogs/main_post_it_dialog.dart';
 import 'package:ssu_meet/widgets/document_duplicate_image.dart';
-import 'package:ssu_meet/widgets/main_page_post_it.dart';
 import 'package:ssu_meet/widgets/registration_button_black.dart';
 import 'package:ssu_meet/widgets/vertical_line_image.dart';
 
@@ -14,6 +14,34 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  // final ScrollController _scrollController = ScrollController();
+  // bool _showScrollButton = false;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+
+  //   _scrollController.addListener(() {
+  //     if (_scrollController.offset >= 200) {
+  //       setState(() {
+  //         _showScrollButton = true;
+  //       });
+  //     } else {
+  //       setState(() {
+  //         _showScrollButton = false;
+  //       });
+  //     }
+  //   });
+  // }
+
+  // void _scrollToTop() {
+  //   _scrollController.animateTo(
+  //     0,
+  //     duration: const Duration(milliseconds: 500),
+  //     curve: Curves.easeInOut,
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
@@ -35,11 +63,11 @@ class _MainPageState extends State<MainPage> {
             ),
             child: Column(
               children: [
-                const SizedBox(
-                  height: 20,
+                SizedBox(
+                  height: screenHeight * 0.02,
                 ),
                 Padding(
-                  padding: const EdgeInsets.all(15.0),
+                  padding: EdgeInsets.all(screenHeight * 0.01),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
@@ -81,10 +109,165 @@ class _MainPageState extends State<MainPage> {
                       return Text('Error: ${snapshot.error}');
                     } else {
                       // If the future completed successfully
-                      return MainPagePostIt(
-                        screenHeight,
-                        snapshot,
-                        screenWidth,
+                      return SizedBox(
+                        height: screenHeight * 0.75,
+                        child: GridView.builder(
+                          // controller: _scrollController,
+                          shrinkWrap: true,
+                          itemCount: snapshot.data!.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
+                            childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
+                            mainAxisSpacing: 10, //수평 Padding
+                            crossAxisSpacing: 10,
+                            //수직 Padding
+                          ),
+                          itemBuilder: (BuildContext context, int index) {
+                            var postIt = snapshot.data![index];
+                            var id = postIt["stickyId"];
+                            var sex = postIt["stickyData"]["sex"];
+                            var birthDate = postIt["stickyData"]["birthDate"];
+                            var age = postIt["stickyData"]["age"];
+                            var college = postIt["stickyData"]["college"];
+                            var major = postIt["stickyData"]["major"];
+                            var height = postIt["stickyData"]["height"];
+                            var instaId = postIt["stickyData"]["instaId"];
+                            var kakaoId = postIt["stickyData"]["kakaoId"];
+                            var phoneNumber =
+                                postIt["stickyData"]["phoneNumber"];
+                            var nickname = postIt["stickyData"]["nickname"];
+                            var mbti = postIt["stickyData"]["mbti"];
+                            var hobbies = postIt["stickyData"]["hobbies"];
+                            var ideals = postIt["stickyData"]["ideals"];
+                            var introduce = postIt["stickyData"]["introduce"];
+
+                            return GestureDetector(
+                              onTap: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return MainPostItDialog(
+                                      nickname: nickname,
+                                      age: age,
+                                      major: major,
+                                      mbti: mbti,
+                                      height: height,
+                                      hobbies: hobbies,
+                                      introduce: introduce,
+                                      instaId: instaId,
+                                      kakaoId: kakaoId,
+                                      phoneNumber: phoneNumber,
+                                      ideals: ideals,
+                                    );
+                                  },
+                                );
+                                print("Tapped Post It (id: $id)");
+                                print("Tapped Post It (age: $age)");
+                                print("Tapped Post It (instaId: $instaId)");
+                              },
+                              child: Center(
+                                child: Container(
+                                  child: Stack(
+                                    alignment: Alignment.center,
+                                    children: <Widget>[
+                                      // First image
+
+                                      Image(
+                                        image: const AssetImage(
+                                            "assets/images/images2/yellowpostit.png"),
+                                        width: screenWidth * 0.4,
+                                        height: screenWidth * 0.4,
+                                      ),
+
+                                      // Second image overlapped on top of the first image
+                                      Positioned(
+                                        left: screenWidth * 0.1,
+                                        top: screenWidth * -0.07,
+                                        child: Image(
+                                          width: screenWidth * 0.2,
+                                          height: screenWidth * 0.2,
+                                          image: const AssetImage(
+                                              "assets/images/images2/tape10.png"),
+                                        ),
+                                      ),
+                                      Positioned(
+                                        top: screenWidth * 0.1,
+                                        left: screenWidth * 0.1,
+                                        child: SizedBox(
+                                          height: screenWidth * 0.2,
+                                          width: screenWidth * 0.2,
+                                          child: SingleChildScrollView(
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  "닉네임: $nickname",
+                                                  style: TextStyle(
+                                                    fontFamily: "Nanum_Ogbice",
+                                                    fontSize:
+                                                        screenWidth * 0.04,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  "나이: $age",
+                                                  style: TextStyle(
+                                                    fontFamily: "Nanum_Ogbice",
+                                                    fontSize:
+                                                        screenWidth * 0.04,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  "MBTI: $mbti",
+                                                  style: TextStyle(
+                                                    fontFamily: "Nanum_Ogbice",
+                                                    fontSize:
+                                                        screenWidth * 0.04,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  "취미 1: ${hobbies[0]}",
+                                                  style: TextStyle(
+                                                    fontFamily: "Nanum_Ogbice",
+                                                    fontSize:
+                                                        screenWidth * 0.04,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  "취미 2: ${hobbies[1]}",
+                                                  style: TextStyle(
+                                                    fontFamily: "Nanum_Ogbice",
+                                                    fontSize:
+                                                        screenWidth * 0.04,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  "취미 3: ${hobbies[2]}",
+                                                  style: TextStyle(
+                                                    fontFamily: "Nanum_Ogbice",
+                                                    fontSize:
+                                                        screenWidth * 0.04,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                        ),
                       );
                     }
                   },
@@ -100,8 +283,15 @@ class _MainPageState extends State<MainPage> {
           ),
         ],
       ),
+      // floatingActionButton: _showScronLocation.miniStartFloat, // 왼쪽 하단에 배치
     );
   }
+
+  // @override
+  // void dispose() {
+  //   _scrollController.dispose();
+  //   super.dispose();
+  // }
 }
 
 // Future<List> getPostItFrontData() async {
--- ssu_meet/lib/widgets/main_page_post_it.dart
+++ ssu_meet/lib/widgets/main_page_post_it.dart
@@ -1,162 +1,166 @@
 import 'package:flutter/material.dart';
 import 'package:ssu_meet/dialogs/main_post_it_dialog.dart';
 
-SizedBox MainPagePostIt(double screenHeight,
-    AsyncSnapshot<List<dynamic>> snapshot, double screenWidth) {
-  ScrollController scrollController = ScrollController();
+Scaffold MainPagePostIt(
+  double screenHeight,
+  AsyncSnapshot<List<dynamic>> snapshot,
+  double screenWidth,
+) {
+  return Scaffold(
+    body: SizedBox(
+      height: screenHeight * 0.7,
+      child: GridView.builder(
+        shrinkWrap: true,
+        itemCount: snapshot.data!.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
+          childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
+          mainAxisSpacing: 10, //수평 Padding
+          crossAxisSpacing: 10,
+          //수직 Padding
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          var postIt = snapshot.data![index];
+          var id = postIt["stickyId"];
 
-  return SizedBox(
-    height: screenHeight * 0.7,
-    child: GridView.builder(
-      shrinkWrap: true,
-      itemCount: snapshot.data!.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, //1 개의 행에 보여줄 item 개수
-        childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
-        mainAxisSpacing: 10, //수평 Padding
-        crossAxisSpacing: 10, //수직 Padding
-      ),
-      itemBuilder: (BuildContext context, int index) {
-        var postIt = snapshot.data![index];
-        var id = postIt["stickyId"];
+          var sex = postIt["stickyData"]["sex"];
+          var birthDate = postIt["stickyData"]["birthDate"];
+          var age = postIt["stickyData"]["age"];
+          var college = postIt["stickyData"]["college"];
+          var major = postIt["stickyData"]["major"];
+          var height = postIt["stickyData"]["height"];
+          var instaId = postIt["stickyData"]["instaId"];
+          var kakaoId = postIt["stickyData"]["kakaoId"];
+          var phoneNumber = postIt["stickyData"]["phoneNumber"];
+          var nickname = postIt["stickyData"]["nickname"];
+          var mbti = postIt["stickyData"]["mbti"];
+          var hobbies = postIt["stickyData"]["hobbies"];
+          var ideals = postIt["stickyData"]["ideals"];
+          var introduce = postIt["stickyData"]["introduce"];
+          // return Text(index.toString());
+          // Future<String> nickname = getPostItFrontNickname(index);
+          // var age = getPostItFrontData(index)["age"];
+          // var mbti = getPostItFrontData(index)["mbti"];
+          // var hobby = getPostItFrontData(index)["hobby"];
+          return GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return MainPostItDialog(
+                    nickname: nickname,
+                    age: age,
+                    major: major,
+                    mbti: mbti,
+                    height: height,
+                    hobbies: hobbies,
+                    introduce: introduce,
+                    instaId: instaId,
+                    kakaoId: kakaoId,
+                    phoneNumber: phoneNumber,
+                    ideals: ideals,
+                  );
+                },
+              );
+              print("Tapped Post It (id: $id)");
+              print("Tapped Post It (age: $age)");
+              print("Tapped Post It (instaId: $instaId)");
+            },
+            child: Center(
+              child: Container(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    // First image
 
-        var sex = postIt["stickyData"]["sex"];
-        var birthDate = postIt["stickyData"]["birthDate"];
-        var age = postIt["stickyData"]["age"];
-        var college = postIt["stickyData"]["college"];
-        var major = postIt["stickyData"]["major"];
-        var height = postIt["stickyData"]["height"];
-        var instaId = postIt["stickyData"]["instaId"];
-        var kakaoId = postIt["stickyData"]["kakaoId"];
-        var phoneNumber = postIt["stickyData"]["phoneNumber"];
-        var nickname = postIt["stickyData"]["nickname"];
-        var mbti = postIt["stickyData"]["mbti"];
-        var hobbies = postIt["stickyData"]["hobbies"];
-        var ideals = postIt["stickyData"]["ideals"];
-        var introduce = postIt["stickyData"]["introduce"];
-        // return Text(index.toString());
-        // Future<String> nickname = getPostItFrontNickname(index);
-        // var age = getPostItFrontData(index)["age"];
-        // var mbti = getPostItFrontData(index)["mbti"];
-        // var hobby = getPostItFrontData(index)["hobby"];
-        return GestureDetector(
-          onTap: () {
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return MainPostItDialog(
-                  nickname: nickname,
-                  age: age,
-                  major: major,
-                  mbti: mbti,
-                  height: height,
-                  hobbies: hobbies,
-                  introduce: introduce,
-                  instaId: instaId,
-                  kakaoId: kakaoId,
-                  phoneNumber: phoneNumber,
-                  ideals: ideals,
-                );
-              },
-            );
-            print("Tapped Post It (id: $id)");
-            print("Tapped Post It (age: $age)");
-            print("Tapped Post It (instaId: $instaId)");
-          },
-          child: Center(
-            child: Container(
-              child: Stack(
-                alignment: Alignment.center,
-                children: <Widget>[
-                  // First image
-
-                  Image(
-                    image: const AssetImage(
-                        "assets/images/images2/yellowpostit.png"),
-                    width: screenWidth * 0.4,
-                    height: screenWidth * 0.4,
-                  ),
-
-                  // Second image overlapped on top of the first image
-                  Positioned(
-                    left: screenWidth * 0.1,
-                    top: screenWidth * -0.07,
-                    child: Image(
-                      width: screenWidth * 0.2,
-                      height: screenWidth * 0.2,
-                      image:
-                          const AssetImage("assets/images/images2/tape10.png"),
+                    Image(
+                      image: const AssetImage(
+                          "assets/images/images2/yellowpostit.png"),
+                      width: screenWidth * 0.4,
+                      height: screenWidth * 0.4,
                     ),
-                  ),
-                  Positioned(
-                    top: screenWidth * 0.1,
-                    left: screenWidth * 0.1,
-                    child: SizedBox(
-                      height: screenWidth * 0.2,
-                      width: screenWidth * 0.2,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "닉네임: $nickname",
-                              style: TextStyle(
-                                fontFamily: "Nanum_Ogbice",
-                                fontSize: screenWidth * 0.04,
-                                color: Colors.black,
+
+                    // Second image overlapped on top of the first image
+                    Positioned(
+                      left: screenWidth * 0.1,
+                      top: screenWidth * -0.07,
+                      child: Image(
+                        width: screenWidth * 0.2,
+                        height: screenWidth * 0.2,
+                        image: const AssetImage(
+                            "assets/images/images2/tape10.png"),
+                      ),
+                    ),
+                    Positioned(
+                      top: screenWidth * 0.1,
+                      left: screenWidth * 0.1,
+                      child: SizedBox(
+                        height: screenWidth * 0.2,
+                        width: screenWidth * 0.2,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "닉네임: $nickname",
+                                style: TextStyle(
+                                  fontFamily: "Nanum_Ogbice",
+                                  fontSize: screenWidth * 0.04,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                            Text(
-                              "나이: $age",
-                              style: TextStyle(
-                                fontFamily: "Nanum_Ogbice",
-                                fontSize: screenWidth * 0.04,
-                                color: Colors.black,
+                              Text(
+                                "나이: $age",
+                                style: TextStyle(
+                                  fontFamily: "Nanum_Ogbice",
+                                  fontSize: screenWidth * 0.04,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                            Text(
-                              "MBTI: $mbti",
-                              style: TextStyle(
-                                fontFamily: "Nanum_Ogbice",
-                                fontSize: screenWidth * 0.04,
-                                color: Colors.black,
+                              Text(
+                                "MBTI: $mbti",
+                                style: TextStyle(
+                                  fontFamily: "Nanum_Ogbice",
+                                  fontSize: screenWidth * 0.04,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                            Text(
-                              "취미 1: ${hobbies[0]}",
-                              style: TextStyle(
-                                fontFamily: "Nanum_Ogbice",
-                                fontSize: screenWidth * 0.04,
-                                color: Colors.black,
+                              Text(
+                                "취미 1: ${hobbies[0]}",
+                                style: TextStyle(
+                                  fontFamily: "Nanum_Ogbice",
+                                  fontSize: screenWidth * 0.04,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                            Text(
-                              "취미 2: ${hobbies[1]}",
-                              style: TextStyle(
-                                fontFamily: "Nanum_Ogbice",
-                                fontSize: screenWidth * 0.04,
-                                color: Colors.black,
+                              Text(
+                                "취미 2: ${hobbies[1]}",
+                                style: TextStyle(
+                                  fontFamily: "Nanum_Ogbice",
+                                  fontSize: screenWidth * 0.04,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                            Text(
-                              "취미 3: ${hobbies[2]}",
-                              style: TextStyle(
-                                fontFamily: "Nanum_Ogbice",
-                                fontSize: screenWidth * 0.04,
-                                color: Colors.black,
+                              Text(
+                                "취미 3: ${hobbies[2]}",
+                                style: TextStyle(
+                                  fontFamily: "Nanum_Ogbice",
+                                  fontSize: screenWidth * 0.04,
+                                  color: Colors.black,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     ),
   );
 }
