a9eb413168ccaab855ca203d3632535f07c5707f
강승민 <rkdtmdals999@naver.com>
2023-08-21 19:25:50 +0900

    MainPage api 구현

--- ssu_meet/lib/pages/main_page.dart
+++ ssu_meet/lib/pages/main_page.dart
@@ -6,6 +6,8 @@ import 'package:ssu_meet/widgets/document_duplicate_image.dart';
 import 'package:ssu_meet/widgets/registration_button_black.dart';
 import 'package:ssu_meet/widgets/vertical_line_image.dart';
 
+import 'package:http/http.dart' as http;
+
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
@@ -17,9 +19,19 @@ class _MainPageState extends State<MainPage> {
   final ScrollController _scrollController = ScrollController();
   bool _showScrollButton = false;
 
+  // 서버에서 데이터 패치
+  List<dynamic> data = [];
+  int page = 0;
+  bool isLoading = false;
+  bool hasMoreData = true; // Flag to track if more data can be fetched
+  int allStickyCount = 0;
+
   @override
   void initState() {
     super.initState();
+    // _scrollController.addListener(_scrollListener);
+    // fetchData(); // Initial data fetch
+    // page += 1;
 
     _scrollController.addListener(() {
       if (_scrollController.offset >= 200) {
@@ -34,12 +46,77 @@ class _MainPageState extends State<MainPage> {
     });
   }
 
+  // // 스크롤이 최하단으로 내려갔을 때를 감지하는 함수
+  // void _scrollListener() {
+  //   if (_scrollController.position.pixels ==
+  //       _scrollController.position.maxScrollExtent) {
+  //     if (hasMoreData) {
+  //       fetchData(); // Fetch more data when reaching the end
+  //       page += 1;
+  //     }
+  //   }
+  // }
+
+  Future<void> fetchData() async {
+    // 만약 이미 패치중이거나 더이상 데이터가 없다면 즉시 리턴
+    if (isLoading || !hasMoreData) return;
+
+    setState(() {
+      isLoading = true;
+    });
+
+    var url = 'http://localhost:8080/v1/members/main?page=$page&size=10';
+
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {"Authorization": "-----"},
+    );
+
+    if (response.statusCode == 200) {
+      final responseData = json.decode(response.body);
+      final stickyData = responseData["data"]["stickyData"];
+
+      // 전체 포스트잇 개수 초기화 (page가 0일 때만)
+      if (page == 0) {
+        allStickyCount = responseData["data"]["allCount"];
+      }
+
+      if (stickyData.isEmpty) {
+        setState(() {
+          hasMoreData = false;
+        });
+      } else {
+        setState(() {
+          data.addAll(stickyData);
+          // page++; // 페이지 += 1
+        });
+      }
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  // 스크롤 최상단으로 가기
   void _scrollToTop() {
     _scrollController.animateTo(
       0,
       duration: const Duration(milliseconds: 500),
       curve: Curves.easeInOut,
     );
+
+    _refresh();
+  }
+
+  // 포스트잇 데이터 새로고침
+  void _refresh() {
+    setState(() {
+      page = 0;
+      data = [];
+      isLoading = false;
+      hasMoreData = true;
+    });
   }
 
   @override
@@ -97,6 +174,169 @@ class _MainPageState extends State<MainPage> {
                     ],
                   ),
                 ),
+
+                // // 서버 연동 시 사용할 포스트잇 데이터 패치 GridView
+                // GridView.builder(
+                //   controller: _scrollController,
+                //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                //     crossAxisCount: 2, // Number of columns in the grid
+                //     childAspectRatio: 1 / 1, //item 의 가로 1, 세로 1 의 비율
+                //     mainAxisSpacing: 10, //수평 Padding
+                //     crossAxisSpacing: 10,
+                //   ),
+                //   itemCount: data.length + (hasMoreData ? 1 : 0),
+                //   itemBuilder: (context, index) {
+                //     var postIt = data[index];
+                //     var stickyId = postIt["stickyId"];
+                //     var sex = postIt["stickyData"]["sex"];
+                //     var birthDate = postIt["stickyData"]["birthDate"];
+                //     var age = postIt["stickyData"]["age"];
+                //     var college = postIt["stickyData"]["college"];
+                //     var major = postIt["stickyData"]["major"];
+                //     var height = postIt["stickyData"]["height"];
+                //     var instaId = postIt["stickyData"]["instaId"];
+                //     var kakaoId = postIt["stickyData"]["kakaoId"];
+                //     var phoneNumber = postIt["stickyData"]["phoneNumber"];
+                //     var nickname = postIt["stickyData"]["nickname"];
+                //     var mbti = postIt["stickyData"]["mbti"];
+                //     var hobbies = postIt["stickyData"]["hobbies"];
+                //     var ideals = postIt["stickyData"]["ideals"];
+                //     var introduce = postIt["stickyData"]["introduce"];
+
+                //     if (index < data.length) {
+                //       return GestureDetector(
+                //         onTap: () {
+                //           showDialog(
+                //             context: context,
+                //             builder: (BuildContext context) {
+                //               return MainPostItDialog(
+                //                 stickyId: stickyId,
+                //                 nickname: nickname,
+                //                 age: age,
+                //                 major: major,
+                //                 mbti: mbti,
+                //                 height: height,
+                //                 hobbies: hobbies,
+                //                 introduce: introduce,
+                //                 instaId: instaId,
+                //                 kakaoId: kakaoId,
+                //                 phoneNumber: phoneNumber,
+                //                 ideals: ideals,
+                //               );
+                //             },
+                //           );
+                //           print("Tapped Post It (id: $stickyId)");
+                //           print("Tapped Post It (age: $age)");
+                //           print("Tapped Post It (instaId: $instaId)");
+                //         },
+                //         child: Center(
+                //           child: Container(
+                //             child: Stack(
+                //               alignment: Alignment.center,
+                //               children: <Widget>[
+                //                 // First image
+
+                //                 Image(
+                //                   image: const AssetImage(
+                //                       "assets/images/images2/yellowpostit.png"),
+                //                   width: screenWidth * 0.4,
+                //                   height: screenWidth * 0.4,
+                //                 ),
+
+                //                 // Second image overlapped on top of the first image
+                //                 Positioned(
+                //                   left: screenWidth * 0.1,
+                //                   top: screenWidth * -0.07,
+                //                   child: Image(
+                //                     width: screenWidth * 0.2,
+                //                     height: screenWidth * 0.2,
+                //                     image: const AssetImage(
+                //                         "assets/images/images2/tape10.png"),
+                //                   ),
+                //                 ),
+                //                 Positioned(
+                //                   top: screenWidth * 0.1,
+                //                   left: screenWidth * 0.1,
+                //                   child: SizedBox(
+                //                     height: screenWidth * 0.2,
+                //                     width: screenWidth * 0.2,
+                //                     child: SingleChildScrollView(
+                //                       child: Column(
+                //                         crossAxisAlignment:
+                //                             CrossAxisAlignment.start,
+                //                         children: [
+                //                           Text(
+                //                             "닉네임: $nickname",
+                //                             style: TextStyle(
+                //                               fontFamily: "Nanum_Ogbice",
+                //                               fontSize: screenWidth * 0.04,
+                //                               color: Colors.black,
+                //                             ),
+                //                           ),
+                //                           Text(
+                //                             "나이: $age",
+                //                             style: TextStyle(
+                //                               fontFamily: "Nanum_Ogbice",
+                //                               fontSize: screenWidth * 0.04,
+                //                               color: Colors.black,
+                //                             ),
+                //                           ),
+                //                           Text(
+                //                             "MBTI: $mbti",
+                //                             style: TextStyle(
+                //                               fontFamily: "Nanum_Ogbice",
+                //                               fontSize: screenWidth * 0.04,
+                //                               color: Colors.black,
+                //                             ),
+                //                           ),
+                //                           Text(
+                //                             "취미 1: ${hobbies[0]}",
+                //                             style: TextStyle(
+                //                               fontFamily: "Nanum_Ogbice",
+                //                               fontSize: screenWidth * 0.04,
+                //                               color: Colors.black,
+                //                             ),
+                //                           ),
+                //                           Text(
+                //                             "취미 2: ${hobbies[1]}",
+                //                             style: TextStyle(
+                //                               fontFamily: "Nanum_Ogbice",
+                //                               fontSize: screenWidth * 0.04,
+                //                               color: Colors.black,
+                //                             ),
+                //                           ),
+                //                           Text(
+                //                             "취미 3: ${hobbies[2]}",
+                //                             style: TextStyle(
+                //                               fontFamily: "Nanum_Ogbice",
+                //                               fontSize: screenWidth * 0.04,
+                //                               color: Colors.black,
+                //                             ),
+                //                           ),
+                //                         ],
+                //                       ),
+                //                     ),
+                //                   ),
+                //                 ),
+                //               ],
+                //             ),
+                //           ),
+                //         ),
+                //       );
+                //     } else if (hasMoreData) {
+                //       if (!isLoading) {
+                //         fetchData(); // Fetch more data when reaching the end
+                //       }
+                //       return const Center(
+                //         child: CircularProgressIndicator(),
+                //       );
+                //     } else {
+                //       return const Center(
+                //         child: Text('더 이상 등록된 포스트잇이 없습니다.'),
+                //       );
+                //     }
+                //   },
+                // ),
                 FutureBuilder(
                   future: getPostItFrontData(),
                   builder:
